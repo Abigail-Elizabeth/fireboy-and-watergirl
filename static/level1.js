0a05ba58ce12e2d39a3bd7390c953df5cfc5f098
Abigail-Elizabeth <abigail13ej@gmail.com>
2025-04-20 03:20:53 +0100

    Update level1.js

--- static/level1.js
+++ static/level1.js
@@ -1,9 +1,10 @@
 let canvas;
 let context;
-let fpsInterval = 1000 / 20;
+let fpsInterval = 1000 / 24;
 let now;
 let then = Date.now();
 
+// Laying out the background using a tileset
 let background = [
     [13, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 14],
     [10, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  8],
@@ -50,9 +51,9 @@ let background = [
 let tilesPerRow = 8;
 let tileSize = 16;
 let tiles = [];
-
 let backgroundImage = new Image();
 
+// Character fireboy
 let fireboy = {
     x: 0,
     y: 0,
@@ -62,10 +63,15 @@ let fireboy = {
     frameY: 0,
     xChange: 0,
     yChange: 0,
-    in_air: false,
-    color: "red"
+    in_air: false
 }
+let fireboyImage = new Image();
+    // Movement booleans
+    let fb_moveLeft = false;
+    let fb_moveUp = false;
+    let fb_moveRight = false;
 
+// Character watergirl
 let watergirl = {
     x: 0,
     y: 0,
@@ -78,46 +84,30 @@ let watergirl = {
     in_air: false,
     color: "blue"
 }
-
-let fireboyImage = new Image();
 let watergirlImage = new Image();
+    // Movement booleans
+    let wg_moveLeft = false;
+    let wg_moveUp = false;
+    let wg_moveRight = false;
+
+// Objects and other stuff
 let objectsImage = new Image();
-
 let floor;
-
-let fb_moveLeft = false;
-let fb_moveUp = false;
-let fb_moveRight = false;
-let wg_moveLeft = false;
-let wg_moveUp = false;
-let wg_moveRight = false;
 
 document.addEventListener("DOMContentLoaded", init, false);
 
 function init() {
+    // Canvas setup
     canvas = document.querySelector("canvas");
     context = canvas.getContext("2d");
-
+    tiles = [];
+    // Characters' positioning or spawning points
     floor = canvas.height - 32;
     fireboy.x = (canvas.width / 2) - fireboy.width;
     fireboy.y = floor - fireboy.height;
-    watergirl.x = (canvas.height / 2) - watergirl.width;
+    watergirl.x = (canvas.height / 2) - watergirl.width;  
     watergirl.y = floor - watergirl.height;
-
-    for (let i = 0; i < background.length; i++) {
-        for (let j = 0; j < 70; j++) {
-            if (background[i][j] >= 0) {
-                let tile = {
-                    x: i * 16,
-                    y: j * 16,
-                    size: 16
-                }
-                tiles.push(tile);
-            }
-        }
-    }
-    console.log(tiles);
-
+    
     window.addEventListener("keydown", activate, false);
     window.addEventListener("keyup", deactivate, false);
 
@@ -129,8 +119,11 @@ function init() {
     ], draw);
 }
 
+// Animation loop
 function draw() {
     window.requestAnimationFrame(draw);
+    
+    // Frame rate check
     let now = Date.now();
     let elapsed = now - then;
     if (elapsed <= fpsInterval) {
@@ -138,13 +131,25 @@ function draw() {
     }
     then = now - (elapsed % fpsInterval);
 
-    // Drawing the background on canvas
+    // Painting the background brown
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "#584c25";  // background color
+    context.fillStyle = "#584c25";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    // drawing out background using tileset
-    for (let r = 0; r < 40; r += 1) {
-        for (let c = 0; c < 70; c += 1) {
+
+    drawBackgroundTiles();
+
+    // Update the characters
+    updateCharacter(fireboy, fb_moveLeft, fb_moveRight, fb_moveUp);
+    updateCharacter(watergirl, wg_moveLeft, wg_moveRight, wg_moveUp);
+
+    drawCharacter(fireboyImage, fireboy);
+    drawCharacter(watergirlImage, watergirl);
+}
+
+// Draws the background onto the canvas
+function drawBackgroundTiles() {
+    for (let r = 0; r < background.length; r++) {
+        for (let c = 0; c < background[r].length; c++) {
             let tile = background[r][c];
             if (tile >= 0) {
                 let tileRow = Math.floor(tile / tilesPerRow);
@@ -155,187 +160,180 @@ function draw() {
             }
         }
     }
-
-    // drawing fireboy
-    context.drawImage(fireboyImage,
-        fireboy.frameX * fireboy.width, fireboy.frameY * fireboy.height, fireboy.width, fireboy.height,
-        fireboy.x, fireboy.y, fireboy.width, fireboy.height);
-    if (fb_moveUp || fireboy.in_air) {      // handling the number of frames
-        fireboy.frameX = (fireboy.frameX + 1) % 6;
-    } else if (!(fb_moveLeft && fb_moveRight) || fb_moveLeft || fb_moveRight) {
-        fireboy.frameX = (fireboy.frameX + 1) % 4;
-    }
-
-    // drawing watergirl
-    context.drawImage(watergirlImage,
-        watergirl.frameX * watergirl.width, watergirl.frameY * watergirl.height, watergirl.width, watergirl.height,
-        watergirl.x, watergirl.y, watergirl.width, watergirl.height);
-    if (wg_moveUp || watergirl.in_air) {      // handling the number of frames
-        watergirl.frameX = (watergirl.frameX + 1) % 6;
-    } else if (!(wg_moveLeft && wg_moveRight) || wg_moveLeft || wg_moveRight) {
-        watergirl.frameX = (watergirl.frameX + 1) % 4;
-    }
-
-    // drawing objects
-
-    
-    // collisions
-    // Reached solid ground
-
-    // fireboy
-
-    for (let tile in tiles) {
-        if (vertical_collision(fireboy, tile)) {
-            // fireboy.y = tile.y;
-            fireboy.yChange = 0;
-            console.log("hi");
-            break;
-        }
-        if (horizontal_collision(fireboy, tile)) {
-            // fireboy.x = tile.x;
-            fireboy.xChange = 0;
-            console.log("sup");
-            break;
-        }
-    }
-
-
-    // for (let tile in tiles) {
-    //     if (collides(fireboy, tile)) {
-    //         // while (collides(fireboy, tile)) {
-    //         //     tile.x -= Math.sign(fireboy.xChange)
-    //         // }
-    //         // fireboy.x = tile.x;
-    //         // fireboy.xChange = 0;
-    //         // break;
-    //         console.log("sup");
-    //     }
-    // }
-
-    // for (let tile in tiles) {
-    //     if (collides(fireboy, tile)) {
-    //         // while (collides(fireboy, tile)) {
-    //         //     tile.y -= Math.sign(fireboy.yChange)
-    //         // }
-    //         // fireboy.y = tile.y;
-    //         // fireboy.yChange = 0;
-    //         // fireboy.in_air = false;
-    //         // break;
-    //         console.log("hi");
-    //     }
-    // }
-
-    // if (fireboy.y + fireboy.height > floor) {
-    //     fireboy.in_air = false;
-    //     fireboy.y = floor - fireboy.height;
-    //     fireboy.yChange = 0;
-    // } else if ((fireboy.y + fireboy.height > canvas.height - (16 * 5) + 4) && (fireboy.x + fireboy.width > canvas.width - (16 * 8) + 4)) {
-    //     fireboy.in_air = false;
-    //     fireboy.y = canvas.height - (16 * 5) + 4 - fireboy.height;
-    //     fireboy.yChange = 0;
-    // }
-
-    // watergirl
-    if (watergirl.y + watergirl.height > floor) {
-        watergirl.in_air = false;
-        watergirl.y = floor - watergirl.height;
-        watergirl.yChange = 0;
-    }
-
-    // handling key presses
-    // fireboy
-    if (fb_moveUp || fireboy.in_air) {
-        if (!fireboy.in_air) {      // checking if he's in the air already
-            fireboy.yChange = fireboy.yChange - 16;
-            fireboy.in_air = true;
-        }
-        if (!(fb_moveLeft || fb_moveRight)) {       // checking if he's jumping up
-            fireboy.frameY = 5;
-        } else if (fb_moveLeft) {       // checking if he's jumping to the left
-            fireboy.frameY = 4;
-            fireboy.xChange = fireboy.xChange - 0.5;
-        } else if (fb_moveRight) {      // checking if he's jumping to the right
-            fireboy.frameY = 3;
-            fireboy.xChange = fireboy.xChange + 0.5;
-        }
-    } else if (!(fb_moveLeft || fb_moveRight || fb_moveUp || fireboy.in_air)) {     // checking to see if he's standing still
-        fireboy.frameY = 0;
-    } else if (fb_moveLeft) {
-        fireboy.frameY = 2;
-        fireboy.xChange = fireboy.xChange - 0.5;
-    } else if (fb_moveRight) {
-        fireboy.frameY = 1;
-        fireboy.xChange = fireboy.xChange + 0.5;
-    }
-
-    // watergirl
-    if (wg_moveUp || watergirl.in_air) {
-        if (!watergirl.in_air) {      // checking if she's in the air already
-            watergirl.yChange = watergirl.yChange - 16;
-            watergirl.in_air = true;
-        }
-        if (!(wg_moveLeft || wg_moveRight)) {       // checking if she's jumping up
-            watergirl.frameY = 5;
-        } else if (wg_moveLeft) {       // checking if she's jumping to the left
-            watergirl.frameY = 4;
-            watergirl.xChange = watergirl.xChange - 0.5;
-        } else if (wg_moveRight) {      // checking if she's jumping to the right
-            watergirl.frameY = 3;
-            watergirl.xChange = watergirl.xChange + 0.5;
-        }
-    } else if (!(wg_moveLeft || wg_moveRight || wg_moveUp || watergirl.in_air)) {     // checking to see if she's standing still
-        watergirl.frameY = 0;
-    } else if (wg_moveLeft) {       // checking to see if she's moving to the left
-        watergirl.frameY = 2;
-        watergirl.xChange = watergirl.xChange - 0.5;
-    } else if (wg_moveRight) {      // checking to see if she's moving to the right
-        watergirl.frameY = 1;
-        watergirl.xChange = watergirl.xChange + 0.5;
-    }
-
-
-    // Stopping going off left or right
-    if (fireboy.x < 12) {
-        fireboy.x = 12;
-    } else if ((fireboy.x + fireboy.width > canvas.width )) {
-        fireboy.x = canvas.width - fireboy.width;
-    }
-    if ((fireboy.x + fireboy.width > canvas.width - (16 * 8) + 4) && (fireboy.y + fireboy.height > canvas.height - (16 * 5) + 4)) {
-        fireboy.x = canvas.width - fireboy.width - (16 * 8) + 4;
-    }
-    
-    if (watergirl.x < 0) {
-        watergirl.x = 0;
-    } else if (watergirl.x + watergirl.width > canvas.width) {
-        watergirl.x = canvas.width - watergirl.width;
-    }
-
-    // Updating the players
-    if (fb_moveLeft || fb_moveRight || fb_moveUp || fireboy.in_air) {
-        fireboy.x = fireboy.x + fireboy.xChange;
-        fireboy.y = fireboy.y + fireboy.yChange;
-    } else {
-            fireboy.xChange = 0;
-            fireboy.yChange = 0;
-    }
-    if (wg_moveLeft || wg_moveRight || wg_moveUp || watergirl.in_air) {
-        watergirl.x = watergirl.x + watergirl.xChange;
-        watergirl.y = watergirl.y + watergirl.yChange;
-    } else {
-            watergirl.xChange = 0;
-            watergirl.yChange = 0;
-    }
-    
-    // Physics
-    
-    fireboy.yChange = fireboy.yChange + 1.5;  // gravity
-    fireboy.xChange = fireboy.xChange * 0.9;  // friction
-    fireboy.yChange = fireboy.yChange * 0.9;  // friction
-    watergirl.yChange = watergirl.yChange + 1.5;  // gravity
-    watergirl.xChange = watergirl.xChange * 0.9;  // friction
-    watergirl.yChange = watergirl.yChange * 0.9;  // friction
 }
 
+// Updating the characters position 
+function updateCharacter(character, moveLeft, moveRight, moveUp) {
+    // Applying physics
+    updatePhysics(character);
+
+    // Checking to see if he's staring to jump
+    if (moveUp && !character.in_air) {
+        character.yChange = -16; // Initial jump height
+        character.in_air = true;
+    }
+
+    // Handle horizontal movement
+    if (moveLeft) {
+        character.xChange = -5;
+        character.frameY = 2;   
+    } else if (moveRight) {
+        character.xChange = 5;
+        character.frameY = 3;   
+    } else if (!character.in_air) {
+        character.frameY = 0;   // Idle animation if not moving left or right, and not in air
+    }
+
+    // Update animations
+    updateAnimation(character, moveLeft, moveRight, moveUp);
+
+    // Update position
+    character.x += character.xChange;
+    character.y += character.yChange;
+
+    // Handle collisions
+    handleCollisions(character);
+}
+
+function updatePhysics(character) {
+    character.yChange = character.yChange + 1.5;  // gravity
+    character.xChange = character.xChange * 0.9;  // friction
+    character.yChange = character.yChange * 0.9;  // air resistance
+}
+
+// Updates the characters' animations
+function updateAnimation(character, moveLeft, moveRight, moveUp) {
+    // Jump animations
+    if (moveUp || character.in_air) {
+        if (!(moveLeft || moveRight)) {
+            character.frameY = 5;  // Jumping up
+        } else if (moveLeft) {
+            character.frameY = 4;  // Jumping left
+        } else if (moveRight) {
+            character.frameY = 3;  // Jumping right
+        }
+        character.frameX = (character.frameX + 1) % 6;  // 6 frames 
+    }
+    // Walking and idle animations
+    else {
+        character.frameX = (character.frameX + 1) % 4;  // 4 frames
+    }
+}
+
+// Finding the tiles surrounding the character and checking if he has collided with any, and then resolving it
+function handleCollisions(character) {
+    const surroundingTiles = getSurroundingTiles(character);
+
+    // Colliding side booleans
+    let collidingBottom = false;
+    let collidingLeft = false;
+    let collidingRight = false;
+    let collidingTop = false;
+
+    for (const tile of surroundingTiles) {
+            if (isColliding(character, tile)) {
+                const side = resolveCollision(character, tile);
+
+                if (side === 'bottom') collidingBottom = true;
+                else if (side === 'top') collidingTop = true;
+                else if (side === 'left') collidingLeft = true;
+                else if (side === 'right') collidingRight = true;
+            }
+    }
+
+    character.in_air = !collidingBottom;
+
+    if (collidingBottom || collidingTop) {
+        character.yChange = 0;
+    } else if (collidingLeft || collidingRight) {
+        character.xChange = 0;
+    }
+}
+
+// Getting the tiles surrounding a character
+function getSurroundingTiles(character) {
+    // Converting character position to tile coordinates
+    const startCol = Math.floor(character.x / tileSize);
+    const endCol = Math.floor((character.x + character.width) / tileSize);
+    const startRow = Math.floor(character.y / tileSize);
+    const endRow = Math.floor((character.y + character.height) / tileSize);
+
+    const surroundingTiles = [];
+
+    // Get all tiles that the character might be colliding with
+    for (let row = startRow - 1; row <= endRow + 1; row++) {
+        for (let col = startCol - 1; col <= endCol + 1; col++) {
+            if (row >= 0 && row < background.length && 
+                col >= 0 && col < background[0].length &&
+                background[row][col]) {
+                    let tile = {
+                        x: col * tileSize,
+                        y: row * tileSize,
+                        width: tileSize,
+                        height: tileSize
+                    }
+                surroundingTiles.push(tile);
+            }
+        }
+    }
+    return surroundingTiles;
+}
+
+function isColliding(character, obj) {
+    return character.x < obj.x + obj.width &&
+           character.x + character.width > obj.x &&
+           character.y < obj.y + obj.height &&
+           character.y + character.height > obj.y;
+}
+
+// 
+function resolveCollision(character, tile) {
+    const overlapX = Math.min(
+        Math.abs(character.x + character.width - tile.x),
+        Math.abs(character.x - (tile.x + tile.width))
+    );
+
+    const overlapY = Math.min(
+        Math.abs(character.y + character.height - tile.y),
+        Math.abs(character.y - (tile.y + tile.height))
+    );
+
+    if (overlapX < overlapY) {
+        // Horizontal collision
+        if (character.x < tile.x) {
+            character.x = tile.x - character.width;
+            return 'right';
+        } else {
+            character.x = tile.x + tile.width;
+            return 'left';
+        }
+    } else {
+        // Vertical collision
+        if (character.y < tile.y) {
+            character.y = tile.y - character.height;
+            return 'bottom';  // standing on top of tile
+        } else {
+            character.y = tile.y + tile.height;
+            return 'top';     // hitting head
+        }
+    }
+}
+
+// drawing the characters
+function drawCharacter(image, character) {
+    context.drawImage(image,
+        character.frameX * character.width,
+        character.frameY * character.height,    
+        character.width,                        
+        character.height,                       
+        character.x,                           
+        character.y,                           
+        character.width,                        
+        character.height                        
+    );
+}
+
+// Turns on movement for characters based on the key presses 
 function activate(event) {
     let key = event.key;
     if (key === "ArrowLeft" ||
@@ -367,6 +365,7 @@ function activate(event) {
     }
 }
 
+// Turns off movement for characters based on the key presses
 function deactivate(event) {
     let key = event.key;
     if (key === "ArrowLeft") {
@@ -387,6 +386,7 @@ function deactivate(event) {
     }
 }
 
+// Confirms that images have been loaded before the animation loop is started
 function load_assets(assets, callback) {
     let num_assets = assets.length;
     let loaded = function() {
@@ -408,73 +408,9 @@ function load_assets(assets, callback) {
         }
         element.src = asset.url;
     }
-
-    // let tiles = [];
-    // for (let i = 0; i < background.length; i++) {
-    //     for (let j = 0; j < i.length; j++) {
-    //         if (background[i][j] >= 0) {
-    //             let tile = {
-    //                 x: i * 16,
-    //                 y: j * 16,
-    //                 size: 16
-    //             }
-    //             tiles.push(tile);
-    //         }
-    //     }
-    // }
-    // console.log(tiles);
 }
 
-function collides(obj1, obj2) {
-    if (obj1.x + obj1.width < obj2.x ||
-        obj2.x + obj2.size < obj1.x ||
-        obj1.y > obj2.y + obj2.size ||
-        obj2.y > obj1.y + obj1.height) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-
-// function horizontal_collision(player, obj) {
-//     if (player.x + player.width == obj.x ||
-//         player.x == obj.x + obj.size) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-// function vertical_collision(player, obj) {
-//     if (player.y == obj.y + obj.size ||
-//         player.y + player.height == obj.y) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-
-
-function horizontal_collision(player, obj) {
-    if (player.x + player.width <= obj.x &&
-        player.x >= obj.x + obj.size) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function vertical_collision(player, obj) {
-    if (player.y >= obj.y + obj.size &&
-        player.y + player.height <= obj.y) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
+// Stops the game
 function stop(outcome_txt) {
     window.removeEventListener("keydown", activate, false);
     window.removeEventListener("keyup", deactivate, false);
